584316e0df448c5c753bcd64f6760aaed97f9b40
Slawek Lysakowski <slawku1@gmail.com>
2022-12-14 21:37:26 +0100

    initial commit

---
+++ src/main/java/io/slaweksapp/korean_Vocab_Tool/KoreanVocabToolApplication.java
@@ -0,0 +1,13 @@
+package io.slaweksapp.korean_Vocab_Tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanVocabToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreanVocabToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/slaweksapp/korean_Vocab_Tool/KoreanVocabToolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.slaweksapp.korean_Vocab_Tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreanVocabToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
